da936987b554135373b3d9e235f40dd0a82442b9
Abhishek Sharma <118094007+Abhi3127@users.noreply.github.com>
2023-02-09 22:09:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { 
+   createBrowserRouter, RouterProvider 
+  } from "react-router-dom";
+import Home from "./components/Home";
+import SearchResult from "./components/SearchResult";
+import Movie from "./components/Movie";
+import { useEffect,useState } from "react";
+import Counter from "./components/Counter";
+import Actor from "./components/Actor";
+
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: '/search/:movie',
+      element:<SearchResult/>
+    },
+    {
+      path: '/movie/:movieId',
+      element:<Movie/>
+    },
+    {
+      path: '/counter',
+      element:<Counter/>
+    },
+    {
+      path: '/actor/:actorId',
+      element:<Actor/>
+    }
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+      
+    </div>
+  );
+}
+
+export default App;
